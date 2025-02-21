77976a078797be5765f02ba6c728328d309af130
Rachit <rs.phantom236@gmail.com>
2025-02-21 18:01:10 +0530

    Find Floor In Int array

---
+++ src/com/dsa/search/questions/FindFloorOfTheNumber.java
@@ -0,0 +1,29 @@
+package com.dsa.search.questions;
+
+public class FindFloorOfTheNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,8,9,12,42,44,46,76,98};
+        int target = 45;
+        floor(arr , target);
+        System.out.println(arr[floor(arr , target)]);
+    }
+
+    private static int floor(int[] arr , int target){
+        int start = 0;
+        int end = arr.length-1;
+        int mid = -1;
+        while(start <= end){
+            mid = start + (end-start)/2;
+            if(target > arr[mid]){
+                start = mid+1;
+            }else if (target < arr[mid]){
+                end = mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return end ;
+    }
+}
